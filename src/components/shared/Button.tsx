8813fdc1fc5b28568871be1def33f0bb6b2d6a2f
Nadeem Khan <140242307+nadeemsangrasi@users.noreply.github.com>
2024-10-13 12:37:02 +0500

    Update Button.tsx

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ text, className }: { text: string; className?: string }) => {
   return (
     <button
-      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${className}`}
+      className={`hover:bg-blue-500 bg-blue-700 text-white font-bold py-2 px-4 rounded ${className}`}
     >
       {text}
     </button>
